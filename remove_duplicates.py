a742d7fbaf811ce2267265341391419ddf8d08e9
Meds Pogi <69465320+MedsAsperin@users.noreply.github.com>
2023-07-30 09:44:38 +0800

    Delete remove_duplicates.py

--- remove_duplicates.py
+++
@@ -1,7 +0,0 @@
-import os
-from more_itertools import unique_everseen
-
-# with open('test.csv', 'r') as f, open('out.csv', 'w') as out_file:
-#     out_file.writelines(unique_everseen(f))
-
-os.remove("test.txt")
